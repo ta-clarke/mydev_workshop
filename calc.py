779e7c6317d8bc59b015bfda979442694136a00f
Teagan Clarke <teagan.clarke@monash.edu>
2023-08-11 11:26:33 +1000

    add comments

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
+# Perform simple arithmetic encoded in an input string:
+# '1 + 2' -> 3, or '1 - 2' -> -1.
+
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
     num0, num1 = int(num0), int(num1)
